df1d110914ae78a03b74878d9e940bf4a6491fd0
Sierra McCahon <smitche4@mail.umw.edu>
2018-06-25 02:08:45 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -39,6 +39,11 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
+  
+  holiday_hash[:spring].each do |holiday, items|
+    items << supply
+    puts items
+  end
 
 end
 
